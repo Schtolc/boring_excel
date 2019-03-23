e4f400f525b830a5637d5a8aef903ee1bd2a4c0d
Pavel Golubev <pavelgolubev@yandex-team.ru>
2019-03-23 20:12:27 +0300

    fixes

--- script.py
+++ script.py
@@ -1,22 +1,21 @@
-from openpyxl import Workbook, load_workbook
+from openpyxl import load_workbook
 
 wb = load_workbook('input.xlsx')
-input = wb.active
+price_st = wb.active
 
 wb2 = load_workbook('output.xlsx')
-output = wb2.active
+price_our = wb2.active
 
-for article in output['E5':'E3646']:
+for article in price_our['E5':'E3947']:
     art = str(article[0].value).strip()
-    for article2 in input['B4':'B4798']:
+    for article2 in price_st['B4':'B4651']:
         art2 = str(article2[0].value).strip()
         if art == art2:
-            output['G' + str(article[0].row)].value = str(input['B' + str(article2[0].row)].value).strip()
-            output['H' + str(article[0].row)].value = str(input['C' + str(article2[0].row)].value).strip()
-            output['I' + str(article[0].row)].value = str(input['D' + str(article2[0].row)].value).strip()
-            tmp = str(input['E' + str(article2[0].row)].value).strip()
-            output['J' + str(article[0].row)].value = '' if tmp == 'None' else tmp
+            price_our['G' + str(article[0].row)].value = str(price_st['B' + str(article2[0].row)].value).strip()
+            price_our['H' + str(article[0].row)].value = str(price_st['C' + str(article2[0].row)].value).strip()
+            price_our['I' + str(article[0].row)].value = str(price_st['D' + str(article2[0].row)].value).strip()
+            tmp = str(price_st['E' + str(article2[0].row)].value).strip()
+            price_our['J' + str(article[0].row)].value = '' if tmp == 'None' else tmp
             break
 
 wb2.save('result.xlsx')
-
